c6f8b5df99a45861c88b559992da26c76307a6b8
Dylan Reynolds <Dylan_Reynolds@progressive.com>
2022-12-15 14:57:59 -0500

    done with the counter component

--- frontend/gifting-frontend/src/app/features/counter/state/effects/counter-data.effects.ts
+++ frontend/gifting-frontend/src/app/features/counter/state/effects/counter-data.effects.ts
@@ -1,26 +1,37 @@
 import { Injectable } from "@angular/core";
 import { Actions, concatLatestFrom, createEffect, ofType } from "@ngrx/effects";
 import { Store } from "@ngrx/store";
-import { map, tap } from "rxjs";
+import { catchError, filter, map, of, tap } from "rxjs";
 import { selectCountCurrent, selectCountData } from "..";
 import { CountData } from "../../models";
 import { CounterCommands, CounterDocuments } from "../actions/count-actions";
 import { CountState, initialState } from "../reducers/count-reducer";
+import {z} from 'zod';
+import { ApplicationEvents } from "../actions/app-actions";
 
 
 @Injectable()
 export class CounterDataEffects{
 
     private readonly COUNT_DATA_KEY = 'count-data';
+    private readonly CountDataSchema = z.object({
+        current: z.number(),
+        by: z.union([z.literal(1), z.literal(3), z.literal(5)])
+    });
+
     // CounterCommandsLoad => ?? => CounterDocuments.counter
-    loadCountData$ = createEffect(() => {
+    loadCountData$ = createEffect(
+        () => {
         return this.actions$.pipe(
             ofType(CounterCommands.load),  // we only care about this action
             map(() => localStorage.getItem(this.COUNT_DATA_KEY)),  // check local storage, this is going to be null or a string
-            map((storedData) => storedData === null ? initialState : JSON.parse(storedData) as CountState),  // CountState
-            map((data) => CounterDocuments.counter({payload: data}))
-        )
-    }, {dispatch: true})
+            filter(storedStuff => !!storedStuff),  // !! means not equal to null -- probably better with storedStuff => storedStuff !== null
+            map(storedString => JSON.parse(storedString || "{}")),  // {} is just making the compiler happy
+            map(obj => this.CountDataSchema.parse(obj) as CountState),
+            map((payload) => CounterDocuments.counter({payload})),
+            catchError(err => of(ApplicationEvents.error({source: 'Counter', message: 'We have ourselves a hacker here', payload: err})))
+        );
+    }, {dispatch: true});
 
     saveCountData$ = createEffect(() => {
         return this.actions$.pipe(
